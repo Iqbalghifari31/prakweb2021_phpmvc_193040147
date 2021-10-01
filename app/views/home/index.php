9918208d1db697096ab21f82ab3786221109bc29
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2021-10-01 22:18:29 +0700

    file yang berisi index pada tampilan home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,9 @@
-
-     <h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+<div class="card w-75">
+  <div class="card-body">
+    <h5 class="card-title">Selamat Datang</h5>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempore vitae optio culpa. Corporis tempora distinctio mollitia consequatur perspiciatis blanditiis ipsam, ratione ut dicta laudantium, est, at nihil iste quam.</p>
     
+  </div>
+</div>
+</div?

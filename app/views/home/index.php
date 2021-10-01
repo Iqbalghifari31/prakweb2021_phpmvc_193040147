c6de42bc2e0192d6a89e344316c9850296ab226a
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2021-10-01 23:22:17 +0700

    file yang berisi index pada tampilan home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,8 +1,8 @@
 <div class="container">
 <div class="card w-75">
   <div class="card-body">
-    <h5 class="card-title">Selamat Datang</h5>
-    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempore vitae optio culpa. Corporis tempora distinctio mollitia consequatur perspiciatis blanditiis ipsam, ratione ut dicta laudantium, est, at nihil iste quam.</p>
+    <h5 class="card-title">Selamat Datang di Website Saya!</h5>
+    <p class="card-text">Halo, nama saya <?= $data['nama']?></p>
     
   </div>
 </div>
